2bb8949038a3856d76b671107cf717e119439cba
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-03-11 15:22:39 +0100

    Game type design done and update landing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,41 @@ import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import SettingScreen from "../screens/settingScreen"
 import HomeScreen from "../screens/gameScreen"
+import GameTypeScreen from "../screens/gameTypeScreen"
+import { SettingsIcon } from "lucide-react-native"
 
 const Navbar = () => {
-const Stack = createStackNavigator()
+  const Stack = createStackNavigator()
+
+  const settings = {
+    headerRight: () => (
+      <SettingsIcon size={24} color="black" style={{ marginRight: 16 }} />
+    ),
+  }
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Game">
-          <Stack.Screen name="Game" component={HomeScreen} />
-          <Stack.Screen name="Settings" component={SettingScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName=" "
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#CAEFFF",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name=" " component={GameTypeScreen} />
+        <Stack.Screen name="Game" component={HomeScreen} options={settings} />
+        <Stack.Screen
+          name="Settings"
+          component={SettingScreen}
+          options={settings}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
