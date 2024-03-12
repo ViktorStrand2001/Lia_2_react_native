6f10fc96236918175a401d04347bae0522fcf239
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-03-12 14:53:12 +0100

    Small view fixes in app

--- App.tsx
+++ App.tsx
@@ -10,42 +10,37 @@ export default function App() {
   const [openLandingPage, setOpenLandingPage] = useState<boolean>(false)
 
   return (
-    <View className="min-w-full min-h-full bg-bgBlue overflow-hidden">
+    <View className="min-w-screen min-h-screen bg-bgBlue overflow-hidden">
       {!openLandingPage ? (
-        <>
-          <View className=" flex flex-col justify-center items-center">
-            <View className=" mt-20">
-              <Logo />
-            </View>
-            <View className="w-full">
-              <Image
-                source={require("./assets/card-stack_1.png")}
-                className="w-full h-[435px]"
-              />
-            </View>
-            <View className="w-full h-20 flex items-center justify-center relativ ">
-              <Text className="text-center text-lg font-semibold  absolute -top-1">
-                Take on challanges with your
-              </Text>
-              <Text className="text-center text-lg font-semibold absolute top-4">
-                friends and see who becomes the
-              </Text>
-              <Text className="text-center text-lg font-semibold absolute top-9">
-                ultimate challnger!
-              </Text>
-            </View>
-            <View>
-              <TouchableOpacity
-                onPress={() => setOpenLandingPage(true)}
-                className="transition duration-500 ease-in-out transform hover:scale-110"
-              >
-                <View className="bg-primarypink h-16 px-16 rounded-2xl flex items-center justify-center border">
-                  <Image source={require("./assets/Right-Arrow.png")} />
-                </View>
-              </TouchableOpacity>
-            </View>
+        <View className=" flex justify-center items-center">
+          <View className="h-32 justify-center items-end pt-16">
+            <Logo />
           </View>
-        </>
+          <View className="w-full">
+            <Image
+              source={require("./assets/card-stack_1.png")}
+              className="w-full h-[470px]"
+            />
+          </View>
+          <View className="w-full h-16 mb- flex items-center justify-center relativ">
+            <Text className="text-center text-lg font-semibold  absolute -top-1">
+              Take on challanges with your
+            </Text>
+            <Text className="text-center text-lg font-semibold absolute top-4">
+              friends and see who becomes the
+            </Text>
+            <Text className="text-center text-lg font-semibold absolute top-9">
+              ultimate challnger!
+            </Text>
+          </View>
+          <View className="h-32 justify-center items">
+            <TouchableOpacity onPress={() => setOpenLandingPage(true)}>
+              <View className="bg-primarypink h-16 px-16 rounded-2xl flex items-center justify-center border">
+                <Image source={require("./assets/Right-Arrow.png")} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
       ) : (
         <Navbar />
       )}
