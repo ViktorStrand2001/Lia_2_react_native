011e32cde002c52118b14340a061622aaf4a33af
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-04-03 10:36:39 +0200

    fixed answer is a boolean insead of string

--- screens/createQuizCardScreen.tsx
+++ screens/createQuizCardScreen.tsx
@@ -13,7 +13,7 @@ const CreatQuizCardScreen = () => {
   const [formData, setFormData] = useState({
     Title: "",
     Context: "",
-    Answer: "",
+    Answer: false,
   })
 
   const handleSubmit = async () => {
@@ -24,9 +24,9 @@ const CreatQuizCardScreen = () => {
       console.log(addDoc)
 
       setFormData({
-        Title:"",
-        Context:"",
-        Answer:""
+        Title: "",
+        Context: "",
+        Answer: false,
       })
     } catch (error) {
       console.error("Error adding card: ", error)
@@ -36,39 +36,50 @@ const CreatQuizCardScreen = () => {
   return (
     <View className="w-full h-full flex justify-center items-center mt-4">
       <ScrollView className="w-full px-6 h-screen">
-        <Text className="text-2xl font-bold mb-5">Make a Card!</Text>
+        <Text className="text-2xl font-bold mb-5 underline capitalize">make a quiz card!</Text>
         <TextInput
           className="border border-gray-300 rounded p-4 mb-4 w-full"
-          placeholder="Title"
+          placeholder="Title:"
           value={formData.Title}
           onChangeText={(text) => setFormData({ ...formData, Title: text })}
         />
         <TextInput
-          placeholder="Context"
+          placeholder="Context:"
           value={formData.Context}
-          onChangeText={(text) =>
-            setFormData({ ...formData, Context: text })
-          }
+          onChangeText={(text) => setFormData({ ...formData, Context: text })}
           multiline
           numberOfLines={4}
           className="border border-gray-300 rounded p-4 mb-4 w-full"
         />
-        <TextInput
-          placeholder="Answer"
-          value={formData.Answer}
-          onChangeText={(text) => setFormData({ ...formData, Answer: text })}
-          multiline
-          numberOfLines={4}
-          className="border border-gray-300 rounded p-4 mb-4 w-full"
-        />
-   
-     
+        <Text className="capitalize underline">Answer:</Text>
+        <View className="flex flex-row items-center">
+          <TouchableOpacity
+            onPress={() => setFormData({ ...formData, Answer: true })}
+            className="mr-4"
+          >
+            <Text
+              className={`capitalize ${formData.Answer ? "text-blue-700 underline" : "text-black"}`}
+            >
+              true
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setFormData({ ...formData, Answer: false })}
+          >
+            <Text
+              className={`capitalize ${!formData.Answer ? "text-blue-700 underline" : "text-black"}`}
+            >
+              false
+            </Text>
+          </TouchableOpacity>
+        </View>
+
         <View className="w-full flex justify-center items-center ">
           <TouchableOpacity
             onPress={handleSubmit}
             className="bg-blue-600 py-2 px-5 rounded flex justify-center items-center w-32 h-12"
           >
-            <Text className="text-white">Create Post</Text>
+            <Text className="text-white">Create Quiz</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
